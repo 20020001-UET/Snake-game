9db81c415c31cb07e9b0e49670e6efb24b521032
Bá Lương <79621768+20020001-UET@users.noreply.github.com>
2021-03-10 16:02:42 +0700

    Update Playing header

--- Playing.cpp
+++ Playing.cpp
@@ -1,25 +1,70 @@
 ///Playing [Source]
 #include "Playing.h"
 
+#include <ctime>
+#include <random>
+
 ///Playing class
-void Playing::init(SDL_Renderer* _renderer)
+Playing::Playing()
+{
+    console = NULL;
+
+    width = new int;
+    height = new int;
+
+    renderer = NULL;
+    window = NULL;
+
+    snake = NULL;
+}
+
+void Playing::init(SDL_Window* _window, SDL_Renderer* _renderer)
 {
     console = new Console("Playing");
 
+    window = _window;
     renderer = _renderer;
+    SDL_GetWindowSize(window, width, height);
 
-    snake = new Snake("Assets/snake.png", renderer, 0, 0, 32, 32);
+    tileMap = new TileMap("map.dat", renderer, (*height)/PIXEL, (*width)/PIXEL);
+
+    if (tileMap->canRender())
+        console->writeLine("Map init done!");
+
+    snake = new Snake(0, 0, 32, 32);
+
+    fruit = new Fruit(10, 8, 32, 32);
+
+    srand(time(NULL));
 
     console->writeLine("Init done!");
 }
 
 void Playing::loop()
 {
+    if (fruit->isEaten())
+    {
+        fruit->genarate();
+    }
+
+    if ((snake->get_tile_x() == fruit->get_tile_x()) && (snake->get_tile_y() == fruit->get_tile_y()) && !fruit->isEaten())
+    {
+        snake->eatFruit();
+        fruit->setEaten();
+    }
+
+    fruit->update();
+
     snake->moving();
 }
 
 void Playing::render()
 {
+    tileMap->render();
+
+    if (!fruit->isEaten())
+        fruit->render();
+
     snake->render();
 }
 
@@ -47,4 +92,23 @@ void Playing::keyPressed(int key_sym)
 void Playing::keyReleased(int key_sym)
 {
     console->writeLine("Key released.");
+}
+
+void Playing::destroy()
+{
+    window = NULL;
+    renderer = NULL;
+
+    delete snake;
+    snake = NULL;
+
+    delete width;
+    width = NULL;
+    delete height;
+    height = NULL;
+
+    console->writeLine("Destroyed Playing State!");
+
+    delete console;
+    console = NULL;
 }
--- Playing.h
+++ Playing.h
@@ -7,24 +7,34 @@
 
 //Project headers
 #include "Console.h"
+#include "Resources.h"
+#include "TileMap.h"
 #include "Snake.h"
+#include "Fruit.h"
 #include "Direction.h"
 
 ///Playing class
 class Playing
 {
     public:
-        void init(SDL_Renderer* _renderer);
+        Playing();
+        void init(SDL_Window* _window, SDL_Renderer* _renderer);
         void loop();
         void render();
         void keyPressed(int key_sym);
         void keyReleased(int key_sym);
+        void destroy();
 
     private:
         Console* console;
+        SDL_Window* window;
         SDL_Renderer* renderer;
 
+        int *width, *height;
+
+        TileMap* tileMap;
         Snake* snake;
+        Fruit* fruit;
 
 };
 
